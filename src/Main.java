e87250cacbf76a65a8b6cc33fe605a21a16ca41a
Neto work <lenas_2003@mail.ru>
2023-05-19 22:55:41 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5050;
+        int rub = 20;
+        int bonus = price / rub;
+
+
+        System.out.println("Начисленные мили за купленный билет:" + bonus);
+    }
+}
+
